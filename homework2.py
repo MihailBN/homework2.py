db7c3c40302ddc8312d60d2be9aea1d07f5c7205
MihailBN <aratparosan@gmail.com>
2024-06-12 13:45:27 +0500

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+homework = 12
+time = 1.5
+name = 'Python'
+Time_per_task = time/homework
+print('курс:', name,', всего задач:',homework,', затрачено часов:',time,', среднее время выполнения',Time_per_task)
